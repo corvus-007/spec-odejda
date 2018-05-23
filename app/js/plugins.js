b70e376e1c0b97caa2a4e474d964bb8a85141802
Denis <corvus007@yandex.ru>
2018-05-23 16:54:18 +0300

    Update 2018-05-23T13:54:13.914Z

--- app/js/plugins.js
+++
@@ -1,2 +0,0 @@
-//=require ../../node_modules/svg4everybody/dist/svg4everybody.js
-//=require ../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js
--- js/modules.js
+++ js/modules.js
@@ -21,3 +21,4 @@ window.util = (function () {
     }
   }
 })();
+
