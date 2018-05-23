b0406b981d0a9cb57abb214a44e4ec9137e5999c
Denis <corvus007@yandex.ru>
2018-05-23 16:53:09 +0300

    first commit

---
+++ app/js/modules/util.js
@@ -0,0 +1,23 @@
+'use strict';
+
+window.util = (function () {
+  return {
+    setMaxHeight: function (selector) {
+      var maxHeight;
+      var elements = document.querySelectorAll(selector);
+
+      if (!elements.length) {
+        return;
+      }
+
+      maxHeight = Array.from(elements).reduce(function findMaxHeight(prevValue, element) {
+        var currentValue = element.offsetHeight;
+        return (prevValue > currentValue) ? prevValue : currentValue;
+      }, 0);
+
+      Array.from(elements).forEach(function specifyMaxHeight(it) {
+        it.style.height = maxHeight + 'px';
+      });
+    }
+  }
+})();
