2fd7c7deb35724a3a9b3721c8658e0f7be014693
Денис <corvus007@yandex.ru>
2018-05-29 16:10:05 +0300

    Добавляет tippy.js

--- app/js/plugins.js
+++ app/js/plugins.js
@@ -1,2 +1,3 @@
 //=require ../../node_modules/svg4everybody/dist/svg4everybody.js
 //=require ../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js
+//=require ../../node_modules/tippy.js/dist/tippy.min.js
