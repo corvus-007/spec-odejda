040d33e844629e26b3143583a33b161ae51b543f
Денис <corvus007@yandex.ru>
2018-07-02 14:12:15 +0300

    Подключает flatpickr

--- app/js/plugins.js
+++ app/js/plugins.js
@@ -3,3 +3,5 @@
 //=require ../../node_modules/tippy.js/dist/tippy.min.js
 //=require ../../node_modules/tabslet/jquery.tabslet.min.js
 //=require ../../node_modules/accordionjs/accordion.min.js
+//=require ../../node_modules/flatpickr/dist/flatpickr.min.js
+//=require ../../node_modules/flatpickr/dist/l10n/ru.js
